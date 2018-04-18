11ae9c19c27a17182ad412ce9356fc536c495882
Kartik kumar <kartik.sckorpio@gmail.com>
2018-04-18 22:07:44 +0530

    Add files via upload

---
+++ SCRIPT/altimeter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class altimeter : MonoBehaviour 
+{
+	public Text ALT;
+	public Text SPEED;
+
+	public float alt;
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		SPEED.text = (speed*100).ToString("f0") + " km/h";
+	    ALT.text = (alt).ToString("f0") + " m";
+	}
+}
